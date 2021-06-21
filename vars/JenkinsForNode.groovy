0f3473c8b9585243aa641764599ef6eb8d5c4616
vveereshvsh <vveereshvsh@gmail.com>
2021-06-20 23:23:43 -0500

    Create JenkinsForNode.groovy

---
+++ vars/JenkinsForNode.groovy
@@ -0,0 +1,46 @@
+
+def call(string repoURl){
+  
+  pipeline{
+    
+    agent any
+    
+    tools{
+     
+      nodejs 'node16'
+      
+    }
+    
+    stages{
+      stage("Checkout SCM"){
+       
+        step{ 
+          
+          git branch: 'master',
+	           url: "${repoURl}"
+   
+        }
+              
+      }
+      stage("Npm Install"){
+        step{
+          
+         sh "npm install" 
+          
+        }
+      }
+    }
+    
+    post{
+      
+      always{
+        
+        echo "Running post condition"
+        
+      }
+      
+    }
+    
+  }
+
+}
